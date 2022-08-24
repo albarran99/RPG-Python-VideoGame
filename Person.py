7c6323d95c3dc3f08fd55d82108414d9d1dfd544
ralbarran1 <ralbarran@colway-08.com>
2022-08-24 14:03:50 +0200

    title menu and load a previous game save is implemented now

--- Person.py
+++ Person.py
@@ -1,14 +1,103 @@
-class Humans:
-    def __init__(self, name: str, age: int, place: str, pv: int, dmg: int) -> None:
+from typing import Any
 
+
+class Humans:
+    def __init__(self, name: str, lvl: int, xp: int, pv: int, dmg: int) -> None:
         self.name = name
-        self.age = age
-        self.place = place
+        self.lvl = lvl
+        self.xp = xp
         self.pv = pv
         self.dmg = dmg
 
+    def set_pv(self, x):
+        self.pv = x
 
-def say_hello(user):
-    print('Hola mi nombre es ' + user.name)
-    print('Tengo ' + str(user.age) + ' años humanos')
-    print('Soy de ' + user.place)
+    def set_xp(self, x):
+        self.xp = x
+
+    def set_dmg(self, x):
+        self.dmg = x
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+    def __getattribute__(self, name: str) -> Any:
+        return super().__getattribute__(name)
+
+
+def xp_achieved(human, xp):
+    human.xp = xp + human.xp
+
+
+def lvl_2(human):
+    if human.xp == 100:
+        human.lvl = human.lvl + 1
+        print(f"¡Bien, tu {human.name} a subido de nivel!, ahora es {human.lvl}")
+        print("¡subida!" + '\n'
+              + 'sube +2 =' + f'{human.pv}' + '\n'
+              + 'sube +4 =' + f'{human.dmg}')
+        human.xp = 0
+    else:
+        pass
+
+
+def lvl_3(human):
+    if human.xp == 200:
+        human.lvl = human.lvl + 1
+        print(f"¡Bien, tu {human.name} a subido de nivel!, ahora es {human.lvl}")
+        print("¡subida!" + '\n'
+              + 'sube +5 =' + f'{human.pv}' + '\n'
+              + 'sube +2 =' + f'{human.dmg}')
+        human.xp = 0
+    else:
+        pass
+
+
+def lvl_4(human):
+    if human.xp == 250:
+        human.lvl = human.lvl + 1
+        print(f"¡Bien, tu {human.name} a subido de nivel!, ahora es {human.lvl}")
+        print("¡subida!" + '\n'
+              + 'sube +3 =' + f'{human.pv}' + '\n'
+              + 'sube +4 =' + f'{human.dmg}')
+        human.xp = 0
+    else:
+        pass
+
+
+def lvl_5(human):
+    if human.xp == 300:
+        human.lvl = human.lvl + 1
+        print(f"¡Bien, tu {human.name} a subido de nivel!, ahora es {human.lvl}")
+        print("¡subida!" + '\n'
+              + 'sube +6 =' + f'{human.pv}' + '\n'
+              + 'sube +5 =' + f'{human.dmg}')
+        human.xp = 0
+    else:
+        pass
+
+
+def level_up(human):
+    if lvl_2(human):
+        human.pv = human.pv + 2
+        human.pv = human.dmg + 4
+
+    if lvl_3(human):
+        human.pv = human.pv + 5
+        human.pv = human.dmg + 2
+
+    if lvl_4(human):
+        human.pv = human.pv + 3
+        human.pv = human.dmg + 4
+
+    if lvl_5(human):
+        human.pv = human.pv + 6
+        human.pv = human.dmg + 5
+
+
+def say_hello(human):
+    print('Hola mi nombre es ' + human.name)
+    print('Tengo ' + str(human.lvl) + ' nivel')
+    print('la experiencia acumulada es ' + str(human.xp))
+    print('mi vida actual es de ' + str(human.pv))
+    print('mi daño es de ' + str(human.dmg))
---
+++ main_menu.py
@@ -0,0 +1,449 @@
+import os
+import random
+import sys
+
+from Person import say_hello, Humans
+from Non_organic import say_hello_nonOrganic, Bionicle
+from Animal import say_hello_animals, Monsters
+
+application_path = os.path.dirname(sys.executable)
+
+character_1 = Humans('salary-man', 1, 0, 20, 4)
+character_2 = Bionicle('vezon', 16, 'Voya nui', 'teletransporte', 20, 4)
+character_3 = Monsters('rathalos', 17, 'Japan', 'llamas', 20, 4)
+character_4 = Monsters('zinogre', 12, 'Japan', 'truenos', 20, 4)
+character_5 = Bionicle('vezok', 16, 'Japan', 'ladron de poder', 20, 4)
+
+number_not_repeat = [1, 2, 3, 4]
+ram_command = random.sample(number_not_repeat, 1)
+
+user_choose_character = None
+
+character_found = None
+
+user_character = None
+
+real_chara = None
+
+computer_character = None
+
+is_computer_character = None
+
+
+def characters_info():
+    say_hello(character_1)
+    print("\n")
+    say_hello_nonOrganic(character_2)
+    print("\n")
+    say_hello_animals(character_3)
+    print("\n")
+    say_hello_animals(character_4)
+    print("\n")
+    say_hello_nonOrganic(character_5)
+    print("\n")
+
+
+def save_game(character_info):
+    global user_choose_character
+    save_path = os.path.dirname(os.path.realpath(__file__)) + '/saves/'
+    print("cargando espera un momento por favor...")
+    user_text = input("Escribe el nombre de la partida..." + '\n')
+    path = save_path + user_text
+    file_to_write = open(path, 'w')
+    character_info = f'{user_choose_character.name}' + '\n' \
+                                                       f'{user_choose_character.lvl}' + '\n' \
+                                                                                        f'{user_choose_character.xp}' + '\n' \
+                                                                                                                        f'{user_choose_character.pv}stg' + '\n' \
+                                                                                                                                                           f'{user_choose_character.dmg}'
+    print("guardando espere por favor")
+    file_to_write.write(character_info)
+    file_to_write.close()
+    print("guardado con exito")
+
+
+def load_file():
+    global real_chara
+    path = os.path.dirname(os.path.realpath(__file__)) + '/saves/'
+    list_saves = os.listdir(path)
+    stats = None
+    for save_name in list_saves:
+        print(f"tus partidas: {save_name}")
+        try:
+            user_save = input('-Elige partida-')
+            real_path = path + user_save
+            stats = open(real_path, 'r')
+        except FileNotFoundError:
+            print("ese nombre de archivo de guardado no existe")
+
+        inf_name = stats.readline(-1)
+
+        lvl_num = stats.readline(-2).strip('\n')
+
+        px_num = stats.readline(-3).strip('\n')
+
+        pv_num = stats.readline(-4).strip('\n')
+
+        dmg_num = stats.readline(-5).strip('\n')
+        load_chara = find_a_character(inf_name.strip('\n'))
+        real_chara = load_chara.__init__(inf_name, int(lvl_num), int(px_num), int(pv_num), int(dmg_num))
+        computer_player()
+
+    return real_chara
+
+
+def find_a_character(name):
+    global character_found, user_choose_character
+
+    if name == 'salary-man':
+        character_found = character_1
+        user_choose_character = character_1
+
+    elif name == 'vezon':
+        character_found = character_2
+        user_choose_character = character_2
+
+    elif name == 'rathalos':
+        character_found = character_3
+        user_choose_character = character_3
+
+    elif name == 'zinogre':
+        character_found = character_4
+        user_choose_character = character_4
+
+    elif name == 'vezok':
+        character_found = character_5
+        user_choose_character = character_5
+
+    return character_found
+
+
+def computer_player():
+    global is_computer_character
+    is_computer_character = chose_character_computer()
+    print('Tu rival sera: ' + is_computer_character.name)
+
+
+def options_menu():
+    print('por favor escriba un comando valido:\n'
+          '- atacar <==> hit\n'
+          '- curarte <==> heal\n'
+          '- habilidad <==> skill\n'
+          '- magia <==> spell\n'
+          '- guardar <==> save\n'
+          '- menu de pausa <==> stats\n'
+          '- salir del programa <==> q')
+
+    user_command = input()
+    user_options(user_command)
+
+    return user_command
+
+
+def user_options(command):
+    # global ram_command
+
+    while command != 'q':
+        if command == 'hit':
+            # print(desglose_number(ram_command))
+            # print(desglose_number(number_not_repeat))
+            enemy_dodge()
+            options_menu()
+            command = input()
+
+        elif command == 'heal':
+            print(
+                user_choose_character.name + ' se va a curar 5 puntos de vida' + f'(tu vida actual: {user_choose_character.pv})')
+            if user_choose_character.pv == 20:
+                print('no te puedes curar, ya tienes la vida hasta arriba')
+            else:
+                user_choose_character.pv = user_choose_character.pv + 5
+                print(f'(tu vida actual: {user_choose_character.pv})')
+            options_menu()
+            command = input()
+
+        elif command == 'save':
+            player_character = how_are_you()
+            save_game(player_character)
+            options_menu()
+            command = input()
+
+        elif command == 'stats':
+            character_user = how_are_you()
+            say_hello(character_user)
+            options_menu()
+            command = input()
+
+        else:
+            user_choose_character.pv = user_choose_character.pv + 5
+            ram_command = random.sample(number_not_repeat, 1)
+            enemy_ia()
+            options_menu()
+            command = input()
+
+    else:
+        print('comando no valido')
+        options_menu()
+        command = input()
+
+
+def run_game():
+    global ram_command
+    global user_choose_character
+    global is_computer_character
+
+    tittle_menu()
+
+
+def enemy_dodge():
+    global ram_command
+    global user_choose_character
+    global is_computer_character
+
+    if desglose_number(ram_command) == 1:
+        is_computer_character.pv = is_computer_character.pv - user_choose_character.dmg
+
+        if is_computer_character.pv <= 0:
+            print(
+                is_computer_character.name + ' Ha sido derrotado' + f'(tu vida actual: {user_choose_character.pv}) \n')
+            user_choose_character.xp = user_choose_character.xp + 30
+            print("Has ganado " + str(30) + f'de experiencia, experiencia actual es: {user_choose_character.xp}')
+            ram_command = random.sample(number_not_repeat, 1)
+            chose_character_computer()
+            user_choose_character.pv = user_choose_character.pv + 10
+            options_menu()
+
+        print('Uff!, eso dolió ahora ' + is_computer_character.name + ' le queda de vida ' + str(
+            is_computer_character.pv) + f'(tu vida actual: {user_choose_character.pv})\n')
+
+        ram_command = random.sample(number_not_repeat, 1)
+        # print(ram_command)
+        enemy_ia()
+
+    elif desglose_number(ram_command) == 2:
+        print('vaya! esquivo tú ataque')
+        ram_command = random.sample(number_not_repeat, 1)
+        # print(ram_command)
+        enemy_ia()
+
+    elif desglose_number(ram_command) == 3:
+        print('Parece que ' + is_computer_character.name + " esta a muy distraido y recive un golpe critico")
+        user_choose_character.dmg = user_choose_character.dmg + 2
+        is_computer_character.pv = is_computer_character.pv - user_choose_character.dmg
+        user_choose_character.dmg = user_choose_character.dmg - 2
+
+        if is_computer_character.pv <= 0:
+            print(
+                is_computer_character.name + ' Ha sido derrotado' + f'(tu vida actual: {user_choose_character.pv}) \n')
+            user_choose_character.xp = user_choose_character.xp + 30
+            print("Has ganado " + str(30) + f'de experiencia, experiencia actual es: {user_choose_character.xp}')
+            ram_command = random.sample(number_not_repeat, 1)
+            chose_character_computer()
+            user_choose_character.pv = user_choose_character.pv + 10
+            options_menu()
+
+        print(
+            "dios mio, eso a sido un terrible golpe a " + is_computer_character.name + f" le queda de vida {is_computer_character.pv} " + '\n')
+        ram_command = random.sample(number_not_repeat, 1)
+        # print(ram_command)
+        enemy_ia()
+
+    elif desglose_number(ram_command) == 4:
+        print('tu ataque se perdio!')
+        ram_command = random.sample(number_not_repeat, 1)
+        # print(ram_command)
+        enemy_ia()
+
+    else:
+        print("los dos adversarios se miran fijamente")
+
+
+def enemy_ia():
+    global ram_command
+    global user_choose_character
+    global computer_character
+
+    if desglose_number(ram_command) == 1:
+        print('genial!, a fallado el ataque' + f'(tu vida actual: {user_choose_character.pv})')
+
+    elif desglose_number(ram_command) == 2:
+        print('Oh no! va a atacar ')
+
+        user_choose_character.pv = user_choose_character.pv - is_computer_character.dmg
+
+        if user_choose_character.pv <= 0:
+            user_choose_character.xp = user_choose_character.xp / 2
+            print(
+                user_choose_character.name + f' Has Muerto (y has perdido la mitad de la xp (tu xp actual: {user_choose_character.xp}))')
+            user_choose_character.pv = user_choose_character.pv + 10
+            new_game_menu()
+
+        print(f'(tu vida actual: {user_choose_character.pv}) \n')
+
+    elif desglose_number(ram_command) == 3:
+        print('Oh no! va a atacar ')
+        print('parece que sera golpe critico!')
+
+        is_computer_character.dmg = is_computer_character.dmg + 2
+        user_choose_character.pv = user_choose_character.pv - is_computer_character.dmg
+        is_computer_character.dmg = is_computer_character.dmg - 2
+
+        if user_choose_character.pv <= 0:
+            user_choose_character.xp = user_choose_character.xp / 2
+            print(
+                user_choose_character.name + f' Has Muerto (y has perdido la mitad de la xp (tu xp actual: {user_choose_character.xp}))')
+            user_choose_character.pv = user_choose_character.pv + 10
+            new_game_menu()
+
+        print('uf! como dolio ese golpe critico ' + f'(tu vida actual: {user_choose_character.pv})')
+
+    elif desglose_number(ram_command) == 4:
+        print('parece que esta distraido')
+
+    else:
+        print(f"{is_computer_character.name} se esta replanteando su vida y como ha acabdo peleando con tigo")
+
+
+def characters_menu():
+    print('elige un personaje: \n'
+          '1.''no disponible''\'\n'
+          '2.vezon\n'
+          '3.rathalos\n'
+          '4.zinogre\n'
+          '5.vezok')
+
+
+def chose_character_user():
+    global user_choose_character
+    global user_character
+
+    if user_choose_character == "salary-man" or user_choose_character == "Saytama":
+        user_character = character_1
+
+    elif user_choose_character == "vezon" or user_choose_character == "Vezon":
+        user_character = character_2
+
+    elif user_choose_character == "rathalos" or user_choose_character == 'Rathalos':
+        user_character = character_3
+
+    elif user_choose_character == "zinogre" or user_choose_character == 'Zinogre':
+        user_character = character_4
+
+    elif user_choose_character == "vezok" or user_choose_character == 'Vezok':
+        user_character = character_5
+
+    return user_character
+
+
+def chose_character_computer():
+    global ram_command
+    global user_choose_character
+    global computer_character
+
+    if desglose_number(ram_command) == 1:
+        computer_character = character_2
+        if computer_character == user_choose_character:
+            print('personaje ya elejido')
+            computer_character = character_3
+
+        else:
+            computer_character = character_2
+
+    if desglose_number(ram_command) == 2:
+        computer_character = character_3
+        if computer_character == user_choose_character:
+            print("personaje ya elejido")
+            computer_character = character_2
+
+        else:
+            computer_character = character_3
+
+    if desglose_number(ram_command) == 3:
+        computer_character = character_4
+        if computer_character == user_choose_character:
+            print("personaje ya elejido")
+            computer_character = character_5
+
+        else:
+            computer_character = character_4
+
+    if desglose_number(ram_command) == 4:
+        computer_character = character_5
+        if computer_character == user_choose_character:
+            print("personaje ya elejido")
+            computer_character = character_4
+
+        else:
+            computer_character = character_5
+
+    return computer_character
+
+
+def how_are_you():
+    global user_choose_character
+    global user_character
+
+    if user_choose_character == "salary-man" or user_choose_character == "Salary-man":
+        user_character = character_1
+
+    elif user_choose_character == "vezon" or user_choose_character == "Vezon":
+        user_character = character_2
+
+    elif user_choose_character == "rathalos" or user_choose_character == 'Rathalos':
+        user_character = character_3
+
+    elif user_choose_character == "zinogre" or user_choose_character == 'Zinogre':
+        user_character = character_4
+
+    elif user_choose_character == "vezok" or user_choose_character == 'Vezok':
+        user_character = character_5
+
+    return user_character
+
+
+def desglose_number(num_list: list):
+    global num
+
+    for num in num_list:
+        return num
+
+
+def new_game_menu():
+    global user_choose_character
+
+    characters_info()
+
+    characters_menu()
+
+    print('Elije personaje')
+
+    user_choose_character = input()
+
+    print('has elejido a ' + user_choose_character)
+
+    user_choose_character = chose_character_user()
+
+    return user_choose_character
+
+
+def tittle_menu():
+    print("Fantastica y los multiples reinos")
+    print("Opciones: \n"
+          "-Empezar-\n"
+          "-continuar-\n"
+          "-salir-")
+    user_command = input()
+    if user_command == 'Empezar':
+        new_game_menu()
+    elif user_command == 'continuar':
+        load_file()
+        options_menu()
+
+    elif user_command == 'salir':
+        sys.exit()
+
+    else:
+        print("comando no valido")
+        user_command = input()
+
+
+run_game()
